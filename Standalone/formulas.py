258edc2dfa31d26addb043becedc1f8e454cc4f5
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-02-26 14:13:31 +0000

    Update formulas.py

--- Standalone/formulas.py
+++ Standalone/formulas.py
@@ -2,8 +2,12 @@ from openpyxl import *
 
 num1 = input("Enter in a number: ")
 
+num2 = input("Enter in another number: ")
+
 book = Workbook()
 
 sheet = book.active
 
 sheet["A1"] = num1
+
+sheet["B1"] = num2
