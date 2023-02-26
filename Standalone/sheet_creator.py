210b2ddd084609be01fb08071757ea898b3982a6
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-02-26 14:03:52 +0000

    Update sheet_creator.py

--- Standalone/sheet_creator.py
+++ Standalone/sheet_creator.py
@@ -1,6 +1,6 @@
 from openpyxl import *
 
-name = input("Ennter in the name of your Excel workbook: ")
+name = input("Enter in the name of your Excel workbook: ")
 
 sheet_name1 = input("Enter in the name of the first sheet: ")
 
@@ -15,3 +15,10 @@ def sheet_creator(file):
   sheet.title = sheet_name1
 
   sheet2 = book.create_sheet(title=sheet_name2)
+
+  book.save(file)
+
+
+if __name__ == '__main__':
+
+  sheet_creator(name)
