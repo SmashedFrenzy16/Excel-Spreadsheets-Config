83429ee9a9d311df36397ec3a1973140a59ee0d4
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-02-26 17:47:55 +0000

    Update formulas.py

--- Standalone/formulas.py
+++ Standalone/formulas.py
@@ -1,5 +1,7 @@
 from openpyxl import *
 
+name = input("Enter in the name of your Excel workbook: ")
+
 num1 = input("Enter in a number: ")
 
 num2 = input("Enter in another number: ")
@@ -15,3 +17,5 @@ sheet["B1"] = num2
 sheet["C1"] = "SUM(A1, B1)"
 
 sheet["D1"] = "IF(A1>B1, \"A1 is greater than B1\", \"B1 is greater than A1\")"
+
+book.save(name)
