6e969d7b42e2fe2cece956f9d0fc1452f7aefb0b
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-07-23 19:06:05 +0000

    Adding 3D pie chart creator tool

--- Program/excel_config.py
+++ Program/excel_config.py
@@ -8,7 +8,41 @@ while True:
 
     if choice == "1":
 
-        pass
+        name = input("Enter in the name of your Excel workbook (including extension): ")
+
+        data = [
+
+            ["Crisps", "Sold"],
+            ["Salty", 100],
+            ["Onion", 94],
+            ["Chilli", 88],
+            ["Chicken", 54],
+            ["Bacon", 21],
+        ]
+
+        book = Workbook()
+
+        sheet = book.active
+
+        for row in data:
+
+            sheet.append(row)
+
+        chart = PieChart3D()
+
+        labels = Reference(sheet, min_col=1, min_row=2, max_row=5)
+
+        data2 = Reference(ws, min_col=2, min_row=1, max_row=5)
+
+        chart.add_data(data2, titles_from_data=True)
+
+        chart.set_categories(labels)
+
+        chart.title = "Most Popular Crisps Flavor"
+
+        sheet.add_chart(chart, "A8")
+
+        book.save(name)
 
     elif choice == "2":
 
