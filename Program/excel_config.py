9e6eda8d8484c3fdcc65bdffd9223b0642736d48
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-07-23 19:03:09 +0000

    Add choice structure with a loop

--- Program/excel_config.py
+++ Program/excel_config.py
@@ -1,2 +1,29 @@
 from openpyxl import *
 from openpyxl.chart import PieChart3D, Reference
+
+while True:
+
+
+    choice = input("Enter in your choice (1, 2, or 3): ")
+
+    if choice == "1":
+
+        pass
+
+    elif choice == "2":
+
+        pass
+
+    elif choice == "3": 
+
+        pass
+
+    else:
+
+        print("Invalid choice!")
+    
+    input("Do you want to select another choice? (y/n): ")
+
+    if choice == "n" or choice == "N":
+
+        break
