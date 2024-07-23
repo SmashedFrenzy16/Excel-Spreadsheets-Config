cd0d3031d107227b04cada566cac70fc0b74c88c
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-07-23 19:07:18 +0000

    Adding the formulas code

--- Program/excel_config.py
+++ Program/excel_config.py
@@ -46,7 +46,25 @@ while True:
 
     elif choice == "2":
 
-        pass
+        name = input("Enter in the name of your Excel workbook (including extension): ")
+
+        num1 = input("Enter in a number: ")
+
+        num2 = input("Enter in another number: ")
+
+        book = Workbook()
+
+        sheet = book.active
+
+        sheet["A1"] = num1
+
+        sheet["B1"] = num2
+
+        sheet["C1"] = "SUM(A1, B1)"
+
+        sheet["D1"] = "IF(A1>B1, \"A1 is greater than B1\", \"B1 is greater than A1\")"
+
+        book.save(name)
 
     elif choice == "3": 
 
